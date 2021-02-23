a300e20ad625dd8ca58f230c3b8f93d6a500f213
Perkyderm <ncgoedecke@gmail.com>
2021-02-22 19:12:44 -0700

    initial .json reading

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const crypto = require("crypto");
-const util = require("util");
 
 //Express
 const app = express();
@@ -13,3 +12,20 @@ const PORT = process.env.PORT || 9080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
+
+//* Global array using Promise to read the JSON
+let savedNotesGlobal = promisify(fs.readFile);
+function getSavedNotes() {
+  console.log("Saved Notes", savedNotesGlobal("./db/db.json", "utf8"));
+  return (savedNotes = savedNotesGlobal("./db/db.json", "utf8"));
+}
+
+//* Reads db.json file and returns all saved notes as JSON
+app.get("/api/notes", (req, res) => {
+  getSavedNotes() // function to read the JSON
+    .then((savedNotes) => {
+      //response from getSavedNotes function
+      res.send(JSON.parse(savedNotes));
+    }) // sends to the front end
+    .catch((err) => res.status(500).json(err));
+});
